7497d316c1dce021909f566c9858637ac6b238c9
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-10-11 17:41:33 +0600

    first marvel heroes android

---
+++ android/marvel_heroes/src/main/java/org/atf/marvel_heroes/heroes/HeroesDataset.kt
@@ -0,0 +1,17 @@
+package org.atf.marvel_heroes.heroes
+
+import androidx.core.graphics.toColorInt
+import org.atf.marvel_heroes.R
+import org.atf.marvel_heroes.heroes.model.Hero
+
+class HeroesDataset {
+
+	fun getHeroes(): List<Hero> = listOf(
+		Hero(1, "Beast", R.drawable.beast, "#000fff".toColorInt()),
+		Hero(2, "Capitan America", R.drawable.captain_america, "#5c3a77".toColorInt()),
+		Hero(3, "Electro", R.drawable.electro, "#edc93f".toColorInt()),
+		Hero(4, "Galactus", R.drawable.galactus, "#a52a2a".toColorInt()),
+		Hero(5, "Star lord", R.drawable.star_lord, "#a52a2a".toColorInt())
+	)
+
+}
