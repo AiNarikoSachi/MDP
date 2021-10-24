faed55641be1d059f2355c7970d40e1f61d36aa7
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-10-24 16:37:44 +0600

    first IOS marvel

---
+++ IOS/heroesMarvel/HeroesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeroesCollectionViewCell.swift
+//  heroesMarvel
+//
+//  Created by Никита Шкуренко on 22.10.2021.
+//
+
+import UIKit
+
+class HeroesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var heroesImageView: UIImageView!
+    
+    @IBOutlet weak var heroesLabelView: UILabel!
+}
