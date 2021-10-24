10f3832dd0cb0b2d928196f7f0127f41eeb344b8
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-10-24 16:39:49 +0600

    first IOS MarvelHeroes

--- android/marvel_heroes/src/main/java/org/ans/marvel_heroes/heroes/model/Hero.kt
+++
@@ -1,6 +0,0 @@
-package org.ans.marvel_heroes.heroes.model
-
-import androidx.annotation.ColorInt
-import androidx.annotation.DrawableRes
-
-class Hero(val id: Int, val name: String, @DrawableRes val avatar: Int, @ColorInt val color: Int)
