51fdc2b407a635c5d9680fbe2c28c79016dd6d5d
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-11-06 21:01:33 +0600

    change MarvelHeroesIOS

---
+++ IOS/IOSHeroesMarvel/IOSHeroesMarvel/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  IOSHeroesMarvel
+//
+//  Created by шкуренко on 06.11.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var HeroesImageView: UIImageView!
+    
+    @IBOutlet weak var HeroesLabeView: UILabel!
+}
