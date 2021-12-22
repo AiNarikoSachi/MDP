2fb36273af0dbbdcb7936466755ca6e213d3eb6b
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-12-22 16:01:56 +0600

    secondflutter

--- FlutterFirstProject/lib/main.dart
+++ FlutterFirstProject/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfirstproject/marvel_info.dart';
+
+import 'marvel.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,13 +18,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Marvel {
-  final String name;
-  final String image;
-
-  Marvel({ required this.name, required this.image});
-}
-
 class MarvelPageView extends StatefulWidget {
   const MarvelPageView({Key? key}) : super(key: key);
 
@@ -33,76 +29,123 @@ class _MyStatelessWidget extends State<MarvelPageView> {
   final controller = PageController(viewportFraction: 0.85);
 
   final List<Marvel> heroes = [
-    Marvel(name: "Blade", image: 'image/Blade.jpg'),
-    Marvel(name: "Ghost_rider", image: 'image/Ghost_rider.jpg'),
-    Marvel(name: "Iron_fist", image: 'image/Iron_fist.jpg'),
-    Marvel(name: "Magneto", image: 'image/Magneto.jpg'),
-    Marvel(name: "Punisher", image: 'image/Punisher.jpg'),
+    Marvel(
+        id: 1009191,
+        name: "Blade",
+        image:
+            'http://i.annihil.us/u/prod/marvel/i/mg/8/a0/523ca6f2b11e4/portrait_incredible.jpg',
+        text:
+            'Himself a half-human/half-vampire, "Daywalker" Eric Brooks has honed his abilities to hunt down the undead who have long terrorized mankind.'),
+    Marvel(
+        id: 1010925,
+        name: "Ghost_rider",
+        image:
+            'http://i.annihil.us/u/prod/marvel/i/mg/c/50/535feaa671cc6/portrait_incredible.jpg',
+        text:
+            'Making a demonic deal to save a loved one, biker Johnny Blaze now finds himself cursed to transform into the embodiment of the Spirit of Vengeance.'),
+    Marvel(
+        id: 1011413,
+        name: "Iron_fist",
+        image:
+            'http://i.annihil.us/u/prod/marvel/i/mg/4/20/53176e89b563e/portrait_incredible.jpg',
+        text:
+            'Trained in ways of martial arts at K\'un-Lun, Danny Rand becomes the Immortal Iron Fist and uses his incredible abilities to defend others.'),
+    Marvel(
+        id: 1009417,
+        name: "Magneto",
+        image:
+            'http://i.annihil.us/u/prod/marvel/i/mg/3/b0/5261a7e53f827/portrait_incredible.jpg',
+        text:
+            'Using his mighty ability to control magnetic fields, the one called Magneto fights to help mutants replace humans as the world\'s dominant species.'),
+    Marvel(
+        id: 1009515,
+        name: "Punisher",
+        image:
+            'http://i.annihil.us/u/prod/marvel/i/mg/3/90/5261675f6b22f/portrait_incredible.jpg',
+        text:
+            'Frank Castle was an honest ex-Marine with a family he loved. But when that family was murdered, he became a vigilante with a uniquely brutal brand of justice.'),
   ];
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.grey
-      ),
-      home: Scaffold(
-
-        body: Stack(
-
-          children:<Widget>[
-            Padding(
-                padding: const EdgeInsets.fromLTRB(70,30,50,0),
-                child: Image.asset('image/marvel.png'),
+    return Material(
+      type: MaterialType.transparency,
+      child: MaterialApp(
+        theme:
+            ThemeData(brightness: Brightness.dark, primaryColor: Colors.grey),
+        home: Scaffold(
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                  width: 200,
+                  margin: const EdgeInsets.all(20),
+                  child: Image.asset('image/marvel.png')),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                child: const Text(
+                  'Choose your hero',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38,
+                      fontWeight: FontWeight.w500),
                 ),
-            const SizedBox(
-              width: 800,
-              height: 300,
-
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(80,130,0,0),
-                child: Text("Choose your hero",style: TextStyle(color: Colors.black, fontSize: 30),),),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 175, 0, 10),
-              child: PageView.builder(
-                  controller: controller,
-                  physics: const BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  pageSnapping: true,
-                  padEnds: true,
-                  itemCount: heroes.length,
-                  itemBuilder: (context, index)=> Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Card(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                      elevation: 15,
-                      clipBehavior: Clip.antiAlias,
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: <Widget>[
-                          Image.asset(
-                            heroes[index].image,
-                            fit: BoxFit.cover,
-                            height: double.infinity,
-                            width: double.infinity,
-                            alignment: Alignment.center,
-                          ),
-                           Container(
-                            margin: const EdgeInsets.fromLTRB(40, 380, 20, 0),
-                            child: Text(
-                              heroes[index].name,
-                              style: const TextStyle(color: Colors.white, fontSize: 50),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: PageView.builder(
+                    controller: controller,
+                    physics: const BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    pageSnapping: true,
+                    padEnds: true,
+                    itemCount: heroes.length,
+                    itemBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        MarvelInfo(
+                                          marvel: heroes[index],
+                                        ))),
+                            child: Hero(
+                              tag: heroes[index].id,
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                elevation: 15,
+                                clipBehavior: Clip.antiAlias,
+                                child: Stack(
+                                  alignment: Alignment.bottomLeft,
+                                  children: <Widget>[
+                                    Image.network(
+                                      heroes[index].image,
+                                      fit: BoxFit.cover,
+                                      height: double.infinity,
+                                      width: double.infinity,
+                                      alignment: Alignment.center,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(16),
+                                      child: Text(
+                                        heroes[index].name,
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 50,
+                                            decoration: TextDecoration.none),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  )),
-            ),   ],
+                        )),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ FlutterFirstProject/lib/marvel_info.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'marvel.dart';
+
+class MarvelInfo extends StatelessWidget {
+  final Marvel marvel;
+
+  const MarvelInfo({Key? key, required this.marvel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.grey),
+      home: Scaffold(
+        body: Hero(
+          tag: marvel.id,
+          child: Stack(fit: StackFit.expand, children: [
+            FittedBox(
+              fit: BoxFit.cover,
+              child: Image.network(marvel.image),
+            ),
+            Positioned(
+              top: 24,
+              left: 8,
+              child: IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  )),
+            ),
+            Container(
+              margin: const EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    marvel.name,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        decoration: TextDecoration.none),
+                  ),
+                  Text(
+                    marvel.text,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        decoration: TextDecoration.none),
+                  )
+                ],
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
