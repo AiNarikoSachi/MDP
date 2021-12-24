b732834888ef6c435d5f2b10754d6236234fc329
AiNarikoSachi <62072724+AiNarikoSachi@users.noreply.github.com>
2021-12-24 16:46:37 +0600

    thirdflutter

--- FlutterFirstProject/lib/main.dart
+++ FlutterFirstProject/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfirstproject/marvel_api.dart';
 import 'package:flutterfirstproject/marvel_info.dart';
 
 import 'marvel.dart';
@@ -28,46 +29,54 @@ class MarvelPageView extends StatefulWidget {
 class _MyStatelessWidget extends State<MarvelPageView> {
   final controller = PageController(viewportFraction: 0.85);
 
-  final List<Marvel> heroes = [
-    Marvel(
-        id: 1009191,
-        name: "Blade",
-        image:
-            'http://i.annihil.us/u/prod/marvel/i/mg/8/a0/523ca6f2b11e4/portrait_incredible.jpg',
-        text:
-            'Himself a half-human/half-vampire, "Daywalker" Eric Brooks has honed his abilities to hunt down the undead who have long terrorized mankind.'),
-    Marvel(
-        id: 1010925,
-        name: "Ghost_rider",
-        image:
-            'http://i.annihil.us/u/prod/marvel/i/mg/c/50/535feaa671cc6/portrait_incredible.jpg',
-        text:
-            'Making a demonic deal to save a loved one, biker Johnny Blaze now finds himself cursed to transform into the embodiment of the Spirit of Vengeance.'),
-    Marvel(
-        id: 1011413,
-        name: "Iron_fist",
-        image:
-            'http://i.annihil.us/u/prod/marvel/i/mg/4/20/53176e89b563e/portrait_incredible.jpg',
-        text:
-            'Trained in ways of martial arts at K\'un-Lun, Danny Rand becomes the Immortal Iron Fist and uses his incredible abilities to defend others.'),
-    Marvel(
-        id: 1009417,
-        name: "Magneto",
-        image:
-            'http://i.annihil.us/u/prod/marvel/i/mg/3/b0/5261a7e53f827/portrait_incredible.jpg',
-        text:
-            'Using his mighty ability to control magnetic fields, the one called Magneto fights to help mutants replace humans as the world\'s dominant species.'),
-    Marvel(
-        id: 1009515,
-        name: "Punisher",
-        image:
-            'http://i.annihil.us/u/prod/marvel/i/mg/3/90/5261675f6b22f/portrait_incredible.jpg',
-        text:
-            'Frank Castle was an honest ex-Marine with a family he loved. But when that family was murdered, he became a vigilante with a uniquely brutal brand of justice.'),
-  ];
+  List<Marvel> heroes = [];
+  bool isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    MarvelApi api = MarvelApi();
+    api.readMarvelHeroes([1009191, 1010925, 1011413, 1009417, 1009368]).then(
+        (values) {
+      setState(() {
+        isLoaded = true;
+        heroes = values;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoaded == true && heroes.isEmpty) {
+      return Material(
+        type: MaterialType.transparency,
+        child: MaterialApp(
+          theme:
+              ThemeData(brightness: Brightness.dark, primaryColor: Colors.grey),
+          home: Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: const [
+                    Text(
+                      'Heroes not found',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
     return Material(
       type: MaterialType.transparency,
       child: MaterialApp(
